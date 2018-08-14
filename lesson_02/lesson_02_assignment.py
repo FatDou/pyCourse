6646f8af3eb9736bc4265279b4650c47c625d3ee
FatDou <pfkmldf4@outlook.com>
2018-08-14 09:22:38 +0800

    fat dou

---
+++ lesson_02/lesson_02_assignment.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 13:49:15 2018
+
+@author: fatdou
+"""
+
+print('\u207c')
+print('\x20\x7c')
+#print (unicode("\u207c", encoding="utf-8"))
---
+++ lesson_03/hw03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 11:09:32 2018
+
+@author: fatdou
+"""
+
